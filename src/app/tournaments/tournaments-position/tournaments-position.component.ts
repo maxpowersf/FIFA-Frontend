edb1afdbdd0ea78f6316481b81309ca59b4fd73b
Paolo Marcolini <canalla88.13@gmail.com>
2020-05-12 17:45:52 -0300

    Upload Positions to Tournaments, part two

--- src/app/tournaments/tournaments-position/tournaments-position.component.ts
+++ src/app/tournaments/tournaments-position/tournaments-position.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Tournament } from '../models/tournament.model';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Team } from 'src/app/teams/team.model';
 import { TeamService } from 'src/app/teams/team.service';
-import { debug } from 'util';
 import { MatSnackBar } from '@angular/material';
+import { Position } from '../models/position.model';
+import { PositionService } from '../services/position.service';
+import { PositionsArray } from '../models/positionsArray.model';
 
 @Component({
   selector: 'app-tournaments-position',
@@ -21,21 +23,12 @@ export class TournamentsPositionComponent implements OnInit {
 
   isQualification: boolean;
 
-  /*get teamp() { return this.positionsForm.get('teamp'); }
-  get wins() { return this.positionsForm.get('wins'); }
-  get draws() { return this.positionsForm.get('draws'); }
-  get loses() { return this.positionsForm.get('loses'); }
-  get favor() { return this.positionsForm.get('favor'); }
-  get against() { return this.positionsForm.get('against'); }
-  get group() { return this.positionsForm.get('group'); }
-  get round() { return this.positionsForm.get('round'); }
-  get qualified() { return this.positionsForm.get('qualified'); }*/
-
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
     private teamService: TeamService,
+    private positionService: PositionService,
     private snackBar: MatSnackBar
   ) { }
 
@@ -81,13 +74,124 @@ export class TournamentsPositionComponent implements OnInit {
     loses: [0, Validators.required],
     favor: [0, Validators.required],
     against: [0, Validators.required],
+    pos: [0],
     group: [''],
-    round: [0],
+    round: [''],
     qualified: [0]
   });
 
   onSubmit = () => {
-    console.log('reactiveForm' , this.positionsForm.value);
+    if (!this.positionsForm.valid) { 
+      this.snackBar.open('Complete todos los campos', '', { 
+        duration: 2000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+
+      return; 
+    }
+
+    const positions: PositionsArray = new PositionsArray();
+    const teamsPositions = this.positionsForm.get('teamsPositions') as FormArray;
+    teamsPositions.controls.forEach((element, index) => {
+      positions.position.push(this.processTeamPositions(element, index));
+    });
+
+    /*this.positionService.add(positions)
+      .subscribe(this.goToList);*/
+  }
+
+  processTeamPositions = (teamPosition: AbstractControl, index: number) => {
+    let position: Position = new Position();
+    position.tournamentId = this.tournament.id;
+    position.teamId = teamPosition.get('teamp').value;
+    position.noPosition = index + 1;
+    position.wins = teamPosition.get('wins').value;
+    position.draws = teamPosition.get('draws').value;
+    position.loses = teamPosition.get('loses').value;
+    position.gamesPlayed = position.wins + position.draws + position.loses;
+    position.goalsFavor = teamPosition.get('favor').value;
+    position.goalsAgainst = teamPosition.get('against').value;
+
+    if(this.isQualification) {
+      position.noPosition = teamPosition.get('pos').value;
+      position.group = teamPosition.get('group').value;
+      position.round = teamPosition.get('round').value;
+      position.qualified = teamPosition.get('qualified').value;
+    }
+
+    position.result = this.getTeamResult(position);
+
+    return position;
+  }
+
+  getTeamResult = (position: Position) => {
+    if(this.isQualification) {
+      switch(position.round) {
+        case "1":
+          return "Primera Ronda";
+        case "2":
+          return "Segunda Ronda";
+        case "3":
+          return "Tercera Ronda";
+      }
+    }
+    else {
+      let positionText: string = "";
+      switch(position.noPosition) {
+        case 1:
+          positionText = "Campeón";
+          break;
+        case 2:
+          positionText = "Sub Campeón";
+          break;
+        case 3:
+          if(this.tournament.noOfTeams < 8) {
+            positionText = "Grupos";
+          }
+          else {
+            positionText = "Tercero";
+          }
+          break;
+        case 4:
+          if(this.tournament.noOfTeams < 8) {
+            positionText = "Grupos";
+          }
+          else {
+            positionText = "Cuarto";
+          }
+          break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+          if(this.tournament.noOfTeams > 8) {
+            positionText = "Cuartos de Final";
+          }
+          else {
+            positionText = "Grupos";
+          }
+          break;
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+          if(this.tournament.noOfTeams > 16) {
+            positionText = "Octavos de Final";
+          }
+          else {
+            positionText = "Grupos";
+          }
+          break;
+        default:
+          positionText = "Grupos";
+      }
+      return positionText;
+    }
   }
 
   goToList = () => this.router.navigate(['tournaments']);
