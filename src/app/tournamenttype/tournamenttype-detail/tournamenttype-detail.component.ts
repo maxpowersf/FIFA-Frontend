cd8da8aa61a3db573299041236db9d893d59cf1d
Paolo Marcolini <canalla88.13@gmail.com>
2020-06-22 10:17:17 -0300

    Issue-28. Added pagination to TournamentType Detail

--- src/app/tournamenttype/tournamenttype-detail/tournamenttype-detail.component.ts
+++ src/app/tournamenttype/tournamenttype-detail/tournamenttype-detail.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Team } from 'src/app/teams/models/team.model';
 import { Router, ActivatedRoute } from '@angular/router';
 import { TournamentType } from '../models/tournamenttype.model';
 import { TeamService } from 'src/app/teams/services/team.service';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-tournamenttype-detail',
@@ -11,35 +12,43 @@ import { TeamService } from 'src/app/teams/services/team.service';
 })
 export class TournamenttypeDetailComponent implements OnInit {
 
+  displayedColumns: string[] = ['noPosition', 'team', 'confederationName', 'titles'];
+  dataSource;
+
   tournamentType: TournamentType;
   tournamentTypeFormat: number;
   teams: Team[];
-  
-  displayedColumns: string[] = ['noPosition', 'team', 'confederationName', 'titles'];
+
+  @ViewChild(MatPaginator, null) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private teamService: TeamService
-  ) { }
-
-  ngOnInit() {
+  ) {
     this.tournamentType = this.route.snapshot.data.tournamenttype;
     this.tournamentTypeFormat = this.tournamentType.format;
+  }
 
+  ngOnInit() {
     this.getTeams();
   }
 
   getTeams() {
     this.teamService.getTeamsWithTitles(this.tournamentType.id)
-    .subscribe((res) => {
-      this.teams = res.map(c => this.mapFromApi(c));
-    });
+      .subscribe((res) => {
+        this.teams = res.map(c => this.mapFromApi(c));
+
+        this.dataSource = new MatTableDataSource(this.teams);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      });
   }
 
-  mapFromApi(element: any){
+  mapFromApi(element: any) {
     var titles = 0;
-    switch(this.tournamentTypeFormat){
+    switch (this.tournamentTypeFormat) {
       case 1:
         titles = element.worldCupQualifications;
         break;
@@ -49,11 +58,11 @@ export class TournamenttypeDetailComponent implements OnInit {
       case 3:
         titles = element.confederationsCupTitles;
         break;
-      case 4: 
+      case 4:
         titles = element.worldCupTitles;
         break;
     }
-    return {...element, titles: titles};
+    return { ...element, titles: titles };
   }
 
   goToList = () => this.router.navigate(['../../'], { relativeTo: this.route })
