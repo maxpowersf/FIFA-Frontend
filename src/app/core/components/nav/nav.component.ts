96b0beb41c3070618d2090db19fbc509d5939c20
Paolo Marcolini <canalla88.13@gmail.com>
2023-07-23 18:59:56 +0200

    Cleaning repository

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
