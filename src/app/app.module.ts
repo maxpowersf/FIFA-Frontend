372793a747156e3dd1342ac44456cc003cd48d95
Paolo Marcolini <canalla88.13@gmail.com>
2023-07-23 19:54:33 +0200

    Update angular/material10 cont

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptorProviders } from './http-interceptors';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import 'hammerjs';
-
-declare var Hammer: any;
-@Injectable()
-export class HammerConfig extends GestureConfig {
-  buildHammer(element: HTMLElement) {
-    return new GestureConfig({ touchAction: 'pan-y' }).buildHammer(element);
-  }
-}
-
 @NgModule({
   declarations: [
     AppComponent
@@ -33,10 +23,6 @@ export class HammerConfig extends GestureConfig {
     SharedModule
   ],
   providers: [
-    {
-      provide: HAMMER_GESTURE_CONFIG,
-      useClass: HammerConfig
-    },
     {
       provide : LocationStrategy, 
       useClass: HashLocationStrategy
